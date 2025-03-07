c8ae008080423b711d565554ca31c491123d241e
Khaled <k.sarhan311@gmail.com>
2025-03-07 20:38:51 +0200

    fix: build error

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import { Experience } from "./components/experience/experience";
 import { HeroSection } from "./components/hero/hero";
 import { Navbar } from "./components/navbar/navbar";
 import { Overview } from "./components/overview/overview";
-import { ScrollIndicator } from "./components/scroll-indicator/scroll-indicator";
 import { Work } from "./components/work/work";
 
 export default function Home() {
   return (
     <>
-      {/* <ScrollIndicator /> */}
       <main>
         <Navbar />
         <HeroSection />
